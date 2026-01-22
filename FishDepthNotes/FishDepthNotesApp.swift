70527e7e39ccb9253a2182069716af242647a5e2
Stepan <sfermin2007@icloud.com>
2026-01-22 15:03:00 +0100

    Initial Commit

---
+++ FishDepthNotes/FishDepthNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FishDepthNotesApp.swift
+//  FishDepthNotes
+//
+//  Created by Stepan Yarikova on 22/1/26.
+//
+
+import SwiftUI
+
+@main
+struct FishDepthNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FishDepthNotesTests/FishDepthNotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  FishDepthNotesTests.swift
+//  FishDepthNotesTests
+//
+//  Created by Stepan Yarikova on 22/1/26.
+//
+
+import Testing
+@testable import FishDepthNotes
+
+struct FishDepthNotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
